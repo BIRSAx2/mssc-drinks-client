6bdfd2273d7362789b876a0da8cf5ed3c00f68e7
BIRSAx2 <Mouhieddinesabir@gmail.com>
2021-02-07 17:34:20 +0100

    Initial commit

---
+++ src/main/java/dev/mouhieddine/msscdrinksclient/MsscDrinksClientApplication.java
@@ -0,0 +1,13 @@
+package dev.mouhieddine.msscdrinksclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscDrinksClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsscDrinksClientApplication.class, args);
+  }
+
+}
